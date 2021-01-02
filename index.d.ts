de40250426be8f3083d970eb1caf30b8e6781b64
Lovell Fuller <github@lovell.info>
2021-01-02 20:40:49 +0000

    Add TypeScript definitions

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+export function asString(key: Buffer, input: Buffer): string;
+
+export function asHexString(key: Buffer, input: Buffer): string;
+
+export function asBuffer(key: Buffer, input: Buffer): Buffer;
+
+export function asUInt32Low(key: Buffer, input: Buffer): number;
+
+export function asUInt32High(key: Buffer, input: Buffer): number;
